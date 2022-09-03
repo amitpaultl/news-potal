8a7531cda8636180dcb526bb6c48fcf52fac571a
amitpaultl <amitpaultl@gmail.com>
2022-09-03 08:27:39 +0600

    set catagorey menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log('out put');
+
+// categories url
+
+const catagoreyUrl = () =>{
+    const  url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => dispalyCatagorey(data.data.news_category))
+}
+
+// categories dispaly
+
+const dispalyCatagorey = (data) =>{
+    const catagoreyMenu = document.getElementById('catagorery-menu');
+
+    data.forEach(element => {
+        console.log(element);
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML = `
+            <li>
+                <a class="nav-link " aria-current="page" href="#">${element.category_name}</a>
+            </li>
+        `
+        catagoreyMenu.appendChild(li);
+    });
+
+}
+
+catagoreyUrl();
